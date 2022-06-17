a2628fdd1fcb0c11105a407a6b11561e2393b973
Nicolas Dimitrijevic <nicolas@croll.fr>
2022-06-17 10:43:34 +0200

    Move Aircon air flow settings in a component

--- interface/src/project/AirConStateWebSocketForm.tsx
+++ interface/src/project/AirConStateWebSocketForm.tsx
@@ -8,12 +8,8 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Grid from "@mui/material/Grid";
 
-import HdrAutoOutlinedIcon from '@mui/icons-material/HdrAutoOutlined';
-import HearingDisabledIcon from '@mui/icons-material/HearingDisabled';
-import TuneIcon from '@mui/icons-material/Tune';
-
 import { WEB_SOCKET_ROOT } from '../api/endpoints';
-import { BlockFormControlLabel, FormLoader, MessageBox, SectionContent } from '../components';
+import { FormLoader, MessageBox, SectionContent } from '../components';
 import Temperature from '../components/inputs/Temperature';
 import AdvancedRegisters from '../components/advanced/AdvancedRegisters';
 import { updateValue, useWs } from '../utils';
@@ -24,18 +20,14 @@ import { AirConState } from './types';
 import { MapsHomeWork } from '@mui/icons-material';
 import AirconMode from '../components/inputs/AirconMode';
 import AirconPower from '../components/inputs/AirconPower';
+import AirconFlow from '../components/inputs/AirconFlow';
 
 export const AIRCON_SETTINGS_WEBSOCKET_URL = WEB_SOCKET_ROOT + "airConState";
 
 const AirConStateWebSocketForm: FC = () => {
   const { connected, updateData, data } = useWs<AirConState>(AIRCON_SETTINGS_WEBSOCKET_URL);
-  const saving = false; // hack
 
-  const updateFormValue = updateValue(updateData);
-
-  function temperatureText(value: number) {
-    return `${value}°C`;
-  }
+  const temperatureText = (value: number) => `${value}°C`;
 
   const content = () => {
     if (!connected || !data) {
@@ -56,59 +48,12 @@ const AirConStateWebSocketForm: FC = () => {
 
           <Temperature value={data.temperature} onChange={(value: number) => { updateData({...data!, temperature: value }) }} />
 
-          <BlockFormControlLabel
-            control={
-              <ToggleButtonGroup
-              exclusive
-              aria-label="Flowspeed Mode"
-              sx={{ margin: 1 }}
-              onChange={(ev, v) => updateData({...data!, flowspeed: parseInt(""+v)}) }
-              >
-              <ToggleButton value={0x11} aria-label="Automatic" selected={data.flowspeed === 0x11}>
-                <HdrAutoOutlinedIcon />
-              </ToggleButton>
-              <ToggleButton value={0x12} aria-label="Silent" selected={data.flowspeed === 0x12}>
-                <HearingDisabledIcon />
-              </ToggleButton>
-              <ToggleButton value={0x05} aria-label="Manual" selected={data.flowspeed < 0x11}>
-                <TuneIcon />
-              </ToggleButton>
-              </ToggleButtonGroup>
-            }
-            label="Flow Mode"
-            labelPlacement='start'
+          <AirconFlow
+            flowspeed={data.flowspeed}
+            verticalswing={data.verticalswing}
+            onChange={(params: object) => updateData({...data!, ...params})}
           />
-          <BlockFormControlLabel
-            control={
-              <Box sx={{ width: 300 }}>
-                  <Slider
-                      aria-label="Flow Speed"
-                      value={data.flowspeed-2}
-                      getAriaValueText={temperatureText}
-                      step={1}
-                      min={1}
-                      max={5}
-                      onChangeCommitted={(ev, v) => updateData({...data!, flowspeed: parseInt(""+v)+2})}
-                  />
-              </Box>
-            }
-            label="Flow Speed"
-            labelPlacement='start'
-            sx={{marginTop: 2}}
-          />
-          <BlockFormControlLabel
-            control={
-              <Switch
-                name="verticalswing"
-                disabled={saving}
-                checked={data.verticalswing}
-                onChange={(ev) => updateData({...data!, verticalswing: ev.target.checked})}
-                color="primary"
-              />
-            }
-            label="Vertical Swing"
-            labelPlacement='start'
-          />
+
           <Accordion>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
