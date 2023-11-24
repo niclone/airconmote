0be52306f6c1c2f7a7b708deb40789948c7bdcb8
Nicolas Dimitrijevic <git-nico@zablabla.net>
2023-11-24 10:31:39 +0100

    [server] use DEBUGCONSOLE c++ directive to set the IP of debugger

--- src/DebugConsole.cpp
+++ src/DebugConsole.cpp
@@ -3,16 +3,17 @@
 WiFiClient D;
 
 void debugloop() {
+#ifdef DEBUGCONSOLE
     if (WiFi.status() == WL_CONNECTED) {
         if (!D.connected()) {
             //Serial.println("Connecting to debug console...");
-            D.connect("192.168.168.10", 5555);
-            //debugclient.connect(IPAddress('192.168.168.10'), 5555);
+            D.connect("DEBUGCONSOLE", 5555);
             if (D.connected()) {
                 //Serial.println("Connected to debug console.");
                 D.println("Welcome to AirconMote debug console!");
             }
         }
     }
+#endif
 }
 
