4cb7d905474387552865620dcea3d28728250a6e
Nicolas Dimitrijevic <git-nico@zablabla.net>
2023-11-24 13:52:21 +0100

    [server] fix DebugConsole IP setting

--- src/DebugConsole.cpp
+++ src/DebugConsole.cpp
@@ -7,7 +7,7 @@ void debugloop() {
     if (WiFi.status() == WL_CONNECTED) {
         if (!D.connected()) {
             //Serial.println("Connecting to debug console...");
-            D.connect("DEBUGCONSOLE", 5555);
+            D.connect(DEBUGCONSOLE, 5555);
             if (D.connected()) {
                 //Serial.println("Connected to debug console.");
                 D.println("Welcome to AirconMote debug console!");
